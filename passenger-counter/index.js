5669e0173e34de5834ac51c44029bf728b434ad9
GodwillB <iamgodwillb@gmail.com>
2022-08-28 01:59:59 +0300

    myDogAge

--- passenger-counter/index.js
+++ passenger-counter/index.js
@@ -1,7 +1,12 @@
 //document.getElementById("count-el").innerText = 6;
 
-let firstBatch = 5;
-let secondBatch = 7;
-let myAge = firstBatch + secondBatch;
+// let firstBatch = 5;
+// let secondBatch = 7;
+// let myAge = firstBatch + secondBatch;
 
-console.log(myAge);
+// console.log(myAge);
+
+let myAge = 24;
+let humanDogRatio = 7;
+let myDogAge = myAge * humanDogRatio;
+console.log(myDogAge);
