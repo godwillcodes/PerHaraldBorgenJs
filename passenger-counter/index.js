231b58f32c1d34c5198b375986e791f61941e10a
GodwillB <iamgodwillb@gmail.com>
2022-08-28 02:13:06 +0300

    Onclick events

--- passenger-counter/index.js
+++ passenger-counter/index.js
@@ -6,7 +6,22 @@
 
 // console.log(myAge);
 
-let myAge = 24;
-let humanDogRatio = 7;
-let myDogAge = myAge * humanDogRatio;
-console.log(myDogAge);
+// let myAge = 24;
+// let humanDogRatio = 7;
+// let myDogAge = myAge * humanDogRatio;
+// console.log(myDogAge);
+
+// let bonusPoints = 50;
+// bonusPoints = bonusPoints + 50;
+// bonusPoints = bonusPoints - 75;
+// bonusPoints = bonusPoints + 45;
+// console.log(bonusPoints);
+
+//initialize as 0
+// listen for clicks on increment button
+// increment the count variable when button is clicked
+// render the updated count variable in the count-el paragraph
+
+function increment() {
+  console.log("The button was clicked");
+}
